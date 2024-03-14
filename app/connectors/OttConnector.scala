a1656f99cf38db4ecc6be7261399627b7759326c
Ewan-James-Donovan <34974028+Ewan-James-Donovan@users.noreply.github.com>
2024-03-14 16:54:45 +0000

    Cleanup

--- app/connectors/OttConnector.scala
+++ app/connectors/OttConnector.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package connectors
 
 import config.FrontendAppConfig
@@ -20,7 +36,7 @@ class OttConnector @Inject()(http: HttpClient, appConfig: FrontendAppConfig)(imp
   )
 
   def getGoodsNomenclatures(comcode: String)(implicit hc: HeaderCarrier): Future[Either[Result, OttResponse]] = {
-    val url = s"${appConfig.ottBaseUrl}${appConfig.ottGreenLanePath}/green_lanes/goods_nomenclatures/${comcode}"
+    val url = s"${appConfig.ottBaseUrl}${appConfig.ottGreenLanePath}${comcode}"
     val responseFuture: Future[HttpResponse] = http.GET[HttpResponse](url = url, headers = setHeaders())
 
     responseFuture.map { httpResponse =>
