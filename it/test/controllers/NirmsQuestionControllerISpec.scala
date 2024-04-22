66fbc875573c42b9d53d4ebdefc2bf9354d8bfaf
mycp98 <marcycalderon@hotmail.co.uk>
2024-04-22 17:05:30 +0100

    TGP-431 added tests in NirmsQuestionISpec

--- it/test/controllers/NirmsQuestionControllerISpec.scala
+++ it/test/controllers/NirmsQuestionControllerISpec.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import helpers.ItTestBase
-import play.api.http.Status.SEE_OTHER
+import play.api.http.Status.{BAD_REQUEST, OK, SEE_OTHER}
 import play.api.libs.ws.{WSClient, WSRequest}
 import play.api.test.Helpers.{await, defaultAwaitTimeout}
 
@@ -9,18 +9,51 @@ class NirmsQuestionControllerISpec extends ItTestBase {
 
   lazy val client: WSClient = app.injector.instanceOf[WSClient]
 
+  private val url = s"http://localhost:$port$appRouteContext/nirms-question"
+
   "Nirms question controller" should {
 
-    //TODO test if auth fails?
+    "returns an error when auth fails" in {
+      noEnrolment
+
+      val request: WSRequest = client.url(url).withFollowRedirects(false)
+
+      val response = await(request.get())
+
+      response.status mustBe SEE_OTHER
+
+    }
+
+    "loads page" in {
+      authorisedUser
+
+      val request: WSRequest = client.url(url).withFollowRedirects(false)
+
+      val response = await(request.get())
+
+      response.status mustBe OK
+
+    }
 
     "returns redirect when submitting valid data" in {
+      authorisedUser
 
-      val url = s"http://localhost:$port$appRouteContext/nirms-question"
-      val request: WSRequest = client.url(url)
+      val request: WSRequest = client.url(url).withFollowRedirects(false)
+
+      val response = await(request.post(Map("value" -> "true")))
+
+      response.status mustBe SEE_OTHER
+
+    }
+
+    "returns bad request when submitting no data" in {
+      authorisedUser
+
+      val request: WSRequest = client.url(url).withFollowRedirects(false)
 
       val response = await(request.post(""))
 
-      response.status mustBe SEE_OTHER
+      response.status mustBe BAD_REQUEST
 
     }
 
