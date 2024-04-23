cc76e7bd5cafd72b9440b3a04c4b3cd95352080c
besscerule <63503574+besscerule@users.noreply.github.com>
2024-04-23 15:41:42 +0100

    TGP-441: integration tests

---
+++ it/test/controllers/CategoryGuidanceControllerISpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import helpers.ItTestBase
+import play.api.http.Status.{OK, SEE_OTHER}
+import play.api.libs.ws.{WSClient, WSRequest}
+import play.api.test.Helpers.{await, defaultAwaitTimeout}
+
+class CategoryGuidanceControllerISpec extends ItTestBase {
+
+  lazy val client: WSClient = app.injector.instanceOf[WSClient]
+
+  private val url = s"http://localhost:$port$appRouteContext/category-guidance"
+
+  "Category Guidance controller" should {
+
+    "returns an error when auth fails" in {
+      noEnrolment
+
+      val request: WSRequest = client.url(url).withFollowRedirects(false)
+
+      val response = await(request.get())
+
+      response.status mustBe SEE_OTHER
+    }
+
+    "loads page" in {
+      authorisedUser
+
+      val request: WSRequest = client.url(url).withFollowRedirects(false)
+
+      val response = await(request.get())
+
+      response.status mustBe OK
+    }
+  }
+}
