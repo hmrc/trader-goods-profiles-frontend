07c6a06d901858ba987b15e7c7566b8ecbae5825
besscerule <63503574+besscerule@users.noreply.github.com>
2024-04-25 11:46:11 +0100

    TGP-680: writing tests

--- test/controllers/actions/SessionRequestActionSpec.scala
+++ test/controllers/actions/SessionRequestActionSpec.scala
@@ -17,6 +17,50 @@
 package controllers.actions
 
 import base.SpecBase
+import models.{Eori, InternalId}
+import models.requests.{AuthorisedRequest, DataRequest}
 import org.scalatestplus.mockito.MockitoSugar
+import play.api.mvc.Result
+import services.SessionService
+import org.mockito.Mockito.when
+import play.api.test.FakeRequest
 
-class SessionRequestActionSpec extends SpecBase with MockitoSugar {}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class SessionRequestActionSpec extends SpecBase with MockitoSugar {
+  class Harness(sessionService: SessionService) extends SessionRequestActionImpl(sessionService) {
+    def callRefine[A](request: AuthorisedRequest[A]): Future[Either[Result, DataRequest[A]]] = refine(request)
+  }
+
+//  "Session Request Action" - {
+//
+//    "when there is no data in the cache" - {
+//
+//      "must set userAnswers to 'None' in the request" in {
+//
+//        val sessionService = mock[SessionService]
+//        when(sessionService.getUserAnswers(InternalId("id"))) thenReturn Future(Right(None))
+//        val action         = new Harness(sessionService)
+//
+//        val result = action.callRefine(AuthorisedRequest(FakeRequest(), InternalId("id"), Eori("eori"))).futureValue
+//
+//        result.leftSide.must not be defined
+//      }
+//    }
+//
+//    "when there is data in the cache" - {
+//
+//      "must build a userAnswers object and add it to the request" in {
+//
+//        val sessionRepository = mock[SessionRepository]
+//        when(sessionRepository.get("id")) thenReturn Future(Some(UserAnswers("id")))
+//        val action            = new Harness(sessionRepository)
+//
+//        val result = action.callTransform(AuthorisedRequest(FakeRequest(), InternalId("id"), Eori("eori"))).futureValue
+//
+//        result.userAnswers mustBe defined
+//      }
+//    }
+//  }
+}
