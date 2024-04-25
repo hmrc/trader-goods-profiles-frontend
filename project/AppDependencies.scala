536efe9f920bc3a9754ca4629692097e887f66f8
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2024-04-25 13:11:42 +0100

    TGP-429: AppDependencies updated

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,23 +4,19 @@ object AppDependencies {
 
   private val bootstrapVersion = "8.3.0"
   private val hmrcMongoVersion = "1.6.0"
-  val scalamockVersion         = "5.2.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "8.5.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "org.mockito"       %% "mockito-scala"              % "1.17.12",
-    "org.scalamock"     %% "scalamock"                  % scalamockVersion
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion
   )
 
   val test                   = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.17.0",
-    "org.mockito"       %% "mockito-scala"           % "1.17.12",
-    "org.scalamock"     %% "scalamock"               % scalamockVersion
+    "org.mockito"       %% "mockito-scala"           % "1.17.12"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
