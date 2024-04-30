b7caebf144c874491f129c6a2ee945fd5f721f1b
besscerule <63503574+besscerule@users.noreply.github.com>
2024-04-30 15:17:13 +0100

    TGP-714: added other views to tests

--- a11y/FrontendAccessibilitySpec.scala
+++ a11y/FrontendAccessibilitySpec.scala
@@ -1,5 +1,7 @@
 import config.FrontendAppConfig
 import org.scalacheck.Arbitrary
+import play.api.data.Forms._
+import play.api.data.Form
 import play.twirl.api.Html
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryList
 import uk.gov.hmrc.scalatestaccessibilitylinter.views.AutomaticAccessibilitySpec
@@ -9,12 +11,15 @@ import viewmodels.govuk.summarylist._
 import views.html.auth.SignedOutView
 class FrontendAccessibilitySpec extends AutomaticAccessibilitySpec {
 
-  val viewPackageName                                 = "views"
-  val layoutClasses                                   = Seq(classOf[Layout])
-  val appConfig: FrontendAppConfig                    = app.injector.instanceOf[FrontendAppConfig]
+  val viewPackageName                                   = "views"
+  val layoutClasses                                     = Seq(classOf[Layout])
+  val appConfig: FrontendAppConfig                      = app.injector.instanceOf[FrontendAppConfig]
   //implicit val arbConfig: Arbitrary[FrontendAppConfig] = fixed(appConfig)
-  val list: SummaryList                               = SummaryListViewModel(rows = Seq.empty)
-  implicit val arbSummaryList: Arbitrary[SummaryList] = fixed(list)
+  val list: SummaryList                                 = SummaryListViewModel(rows = Seq.empty)
+  implicit val arbSummaryList: Arbitrary[SummaryList]   = fixed(list)
+  private val booleanForm: Form[Boolean]                = Form("value" -> boolean)
+  implicit val arbForm: Arbitrary[Form[_]]              = fixed(booleanForm)
+  implicit val arbBooleanForm: Arbitrary[Form[Boolean]] = fixed(booleanForm)
 
   override def renderViewByClass: PartialFunction[Any, Html] = {
     case profileSetupView: ProfileSetupView                           => render(profileSetupView)
@@ -26,6 +31,8 @@ class FrontendAccessibilitySpec extends AutomaticAccessibilitySpec {
     case unauthorisedView: UnauthorisedView                           => render(unauthorisedView)
     case dummyView: DummyView                                         => render(dummyView)
     case signedOutView: SignedOutView                                 => render(signedOutView)
+    case nirmsQuestionView: NirmsQuestionView                         => render(nirmsQuestionView)
+    case ukimsNumberView: UkimsNumberView                             => render(ukimsNumberView)
   }
   runAccessibilityTests()
 }
