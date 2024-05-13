199529239424450ece24840d06d57d13659996fd
besscerule <63503574+besscerule@users.noreply.github.com>
2024-05-13 16:42:51 +0100

    added validation no tests and broken link

--- app/controllers/CheckYourAnswersController.scala
+++ app/controllers/CheckYourAnswersController.scala
@@ -18,12 +18,18 @@ package controllers
 
 import com.google.inject.Inject
 import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import models.{CheckMode, NormalMode, UserAnswers}
+import models.requests.DataRequest
+import pages.{HasNiphlPage, HasNirmsPage, NiphlNumberPage, NirmsNumberPage, UkimsNumberPage}
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
+import uk.gov.hmrc.play.bootstrap.binders.RedirectUrl
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import viewmodels.checkAnswers.{HasNiphlSummary, HasNirmsSummary, NiphlNumberSummary, NirmsNumberSummary, UkimsNumberSummary}
 import viewmodels.govuk.summarylist._
 import views.html.CheckYourAnswersView
+
+import scala.language.postfixOps
 
 class CheckYourAnswersController @Inject()(
                                             override val messagesApi: MessagesApi,
@@ -37,16 +43,36 @@ class CheckYourAnswersController @Inject()(
   def onPageLoad(): Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
 
-      val list = SummaryListViewModel(
-        rows = Seq(
-          UkimsNumberSummary.row(request.userAnswers),
-          HasNirmsSummary.row(request.userAnswers),
-          NirmsNumberSummary.row(request.userAnswers),
-          HasNiphlSummary.row(request.userAnswers),
-          NiphlNumberSummary.row(request.userAnswers)
-        ).flatten
-      )
+      validateUserAnswers(request.userAnswers) match {
+        case Right(true) =>
+          val list = SummaryListViewModel(
+            rows = Seq(
+              UkimsNumberSummary.row(request.userAnswers),
+              HasNirmsSummary.row(request.userAnswers),
+              NirmsNumberSummary.row(request.userAnswers),
+              HasNiphlSummary.row(request.userAnswers),
+              NiphlNumberSummary.row(request.userAnswers)
+            ).flatten
+          )
+          Ok(view(list))
+      }
+  }
 
-      Ok(view(list))
+  def validateUserAnswers(userAnswers: UserAnswers): Either[Result, Boolean] = userAnswers match {
+    case x if x.get(UkimsNumberPage).isEmpty =>
+      Left(Redirect(routes.JourneyRecoveryController.onPageLoad(Some(RedirectUrl(routes.UkimsNumberController.onPageLoad(NormalMode).url)))))
+    case x if x.get(HasNirmsPage).isEmpty =>
+      Left(Redirect(routes.JourneyRecoveryController.onPageLoad(Some(RedirectUrl(routes.UkimsNumberController.onPageLoad(NormalMode).url)))))
+    case x if x.get(HasNiphlPage).isEmpty =>
+      Left(Redirect(routes.JourneyRecoveryController.onPageLoad(Some(RedirectUrl(routes.UkimsNumberController.onPageLoad(NormalMode).url)))))
+    case x if x.get(HasNirmsPage).contains(true) && x.get(NirmsNumberPage).isEmpty =>
+      Left(Redirect(routes.JourneyRecoveryController.onPageLoad(Some(RedirectUrl(routes.UkimsNumberController.onPageLoad(NormalMode).url)))))
+    case x if x.get(HasNiphlPage).contains(true) && x.get(NiphlNumberPage).isEmpty =>
+      Left(Redirect(routes.JourneyRecoveryController.onPageLoad(Some(RedirectUrl(routes.UkimsNumberController.onPageLoad(NormalMode).url)))))
+    case x if x.get(HasNirmsPage).isEmpty && x.get(NirmsNumberPage).isDefined =>
+      Left(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+    case x if x.get(HasNiphlPage).isEmpty && x.get(NiphlNumberPage).isDefined =>
+      Left(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+    case _ => Right(true)
   }
 }
