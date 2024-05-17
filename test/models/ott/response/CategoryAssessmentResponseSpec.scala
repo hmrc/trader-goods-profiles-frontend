5705be454b779ad313bdad2bbdb9dfb0ad29f6f4
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2024-05-17 09:05:43 +0100

    Model response from OTT Green Lanes API

---
+++ test/models/ott/response/CategoryAssessmentResponseSpec.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.ott
+
+import models.ott.response.{CategoryAssessmentResponse, ExemptionResponse}
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import play.api.libs.json.{JsSuccess, Json}
+
+class CategoryAssessmentResponseSpec extends AnyFreeSpec with Matchers {
+
+  ".reads" - {
+
+    "must deserialise valid JSON with no exemptions" in {
+
+      val json = Json.obj(
+        "id"            -> "abc",
+        "type"          -> "category_assessment",
+        "relationships" -> Json.obj(
+          "exemptions" -> Json.obj(
+            "data" -> Json.arr()
+          ),
+          "theme"      -> Json.obj(
+            "data" -> Json.obj(
+              "id"   -> "1",
+              "type" -> "theme"
+            )
+          )
+        )
+      )
+
+      val result = json.validate[CategoryAssessmentResponse]
+      result mustEqual JsSuccess(CategoryAssessmentResponse("abc", "1", Nil))
+    }
+
+    "must deserialise valid JSON with exemptions" in {
+
+      val json = Json.obj(
+        "id"            -> "abc",
+        "type"          -> "category_assessment",
+        "relationships" -> Json.obj(
+          "exemptions" -> Json.obj(
+            "data" -> Json.arr(
+              Json.obj(
+                "id"   -> "cert",
+                "type" -> "certificate"
+              )
+            )
+          ),
+          "theme"      -> Json.obj(
+            "data" -> Json.obj(
+              "id"   -> "1",
+              "type" -> "theme"
+            )
+          )
+        )
+      )
+
+      val result = json.validate[CategoryAssessmentResponse]
+      result mustEqual JsSuccess(CategoryAssessmentResponse("abc", "1", Seq(ExemptionResponse("cert", "certificate"))))
+    }
+  }
+}
