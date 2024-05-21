533f83bc981ec2859398aa663ab89532f1656a9f
vinoocs <158105847+vinoocs@users.noreply.github.com>
2024-05-21 10:54:28 +0100

    TGP-1003: Scalafmt

--- app/controllers/CreateRecordStartController.scala
+++ app/controllers/CreateRecordStartController.scala
@@ -22,7 +22,6 @@ import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.CreateRecordStartView
-
 import javax.inject.Inject
 
 class CreateRecordStartController @Inject() (
