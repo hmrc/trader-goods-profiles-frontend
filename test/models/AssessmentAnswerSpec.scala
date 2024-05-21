9883013a828cae667312d6d2722ef565e4e000fb
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2024-05-21 16:14:31 +0100

    Add model for an assessment answer

---
+++ test/models/AssessmentAnswerSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import play.api.libs.json.{JsString, JsSuccess, Json}
+
+class AssessmentAnswerSpec extends AnyFreeSpec with Matchers {
+
+  "AssessmentAnswer" - {
+
+    "must serialise / deserialise to / from JSON for an exemption" in {
+
+      val exemption = AssessmentAnswer.Exemption("foo")
+      val json = Json.toJson[AssessmentAnswer](exemption)
+
+      json mustEqual JsString("foo")
+      json.validate[AssessmentAnswer] mustEqual JsSuccess(exemption)
+    }
+
+    "must serialise / deserialise to / from JSON for no exemption" in {
+
+      val json = Json.toJson[AssessmentAnswer](AssessmentAnswer.NoExemption)
+
+      json mustEqual JsString("none")
+      json.validate[AssessmentAnswer] mustEqual JsSuccess(AssessmentAnswer.NoExemption)
+    }
+  }
+}
