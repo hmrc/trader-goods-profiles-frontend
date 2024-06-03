b3f48186a04851eee7ac98d2f9699709fea3cb3d
Ewan-James-Donovan <34974028+Ewan-James-Donovan@users.noreply.github.com>
2024-06-03 11:57:05 +0100

    TGP-1139: Use case classes

--- app/models/helper/CategorisationHelper.scala
+++ app/models/helper/CategorisationHelper.scala
@@ -4,15 +4,17 @@ import models.UserAnswers
 
 object CategorisationHelper {
 
-  def getScenario(userAnswers: UserAnswers): Scenario.Value = {
+  def getScenario(userAnswers: UserAnswers): Scenario = {
     //TODO: Implement logic
-    Scenario.Category1NoExemptions
+    Category1NoExemptions
   }
 
 }
 
-object Scenario extends Enumeration {
-  type Scenario = Value
+sealed trait Scenario
 
-  val Category1NoExemptions, Category3NoAssessments, StandardNoSupplementaryUnits, Category1, Category2 = Value
-}
+case object Category1NoExemptions extends Scenario
+case object Category3NoAssessments extends Scenario
+case object StandardNoSupplementaryUnits extends Scenario
+case object Category1 extends Scenario
+case object Category2 extends Scenario
