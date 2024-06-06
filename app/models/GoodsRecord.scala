bc0aae05433a3b21ac079b647881676fd0e8e1bf
Ewan-James-Donovan <34974028+Ewan-James-Donovan@users.noreply.github.com>
2024-06-06 15:51:31 +0100

    TGP-1236: Remove import

--- app/models/GoodsRecord.scala
+++ app/models/GoodsRecord.scala
@@ -18,7 +18,6 @@ package models
 
 import cats.data.{EitherNec, NonEmptyChain}
 import cats.implicits._
-import models.router.responses.GetGoodsRecordResponse
 import pages._
 import play.api.libs.json.{Json, OFormat}
 import queries.CommodityQuery
