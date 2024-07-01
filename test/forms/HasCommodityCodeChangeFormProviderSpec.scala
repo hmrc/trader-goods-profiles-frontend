25109d0fc770eb298683d0478e26e116a5cd554b
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2024-07-01 13:46:29 +0100

    TGP-1385: scaffolds pages added

---
+++ test/forms/HasCommodityCodeChangeFormProviderSpec.scala
@@ -0,0 +1,29 @@
+package forms
+
+import forms.behaviours.BooleanFieldBehaviours
+import play.api.data.FormError
+
+class HasCommodityCodeChangeFormProviderSpec extends BooleanFieldBehaviours {
+
+  val requiredKey = "hasCommodityCodeChange.error.required"
+  val invalidKey = "error.boolean"
+
+  val form = new HasCommodityCodeChangeFormProvider()()
+
+  ".value" - {
+
+    val fieldName = "value"
+
+    behave like booleanField(
+      form,
+      fieldName,
+      invalidError = FormError(fieldName, invalidKey)
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+  }
+}
