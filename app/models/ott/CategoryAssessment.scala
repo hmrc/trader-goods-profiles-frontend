3f26ec1687ccc4ecebad6c5c3ac03d12f5fde887
Chris Cook <114592674+ChrisCookOC@users.noreply.github.com>
2024-07-15 10:50:29 +0100

    Parse categorisation when other exemption.

--- app/models/ott/CategoryAssessment.scala
+++ app/models/ott/CategoryAssessment.scala
@@ -52,6 +52,11 @@ object CategoryAssessment {
         ottResponse.additionalCodes
           .find(_.id == id)
           .map(x => AdditionalCode(x.id, x.code, x.description))
+
+      case ExemptionType.OtherExemption =>
+        ottResponse.otherExemptions
+        .find(_.id == id)
+        .map(x => OtherExemption(x.id, x.code, x.description))
     }
 
   implicit lazy val format: OFormat[CategoryAssessment] = Json.format
