63d3cb626fc11fec554cb56cc89123869f3cb5ca
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2024-07-29 10:56:08 +0100

    TGP-2013: tidy up

--- app/controllers/CategoryGuidanceController.scala
+++ app/controllers/CategoryGuidanceController.scala
@@ -26,7 +26,6 @@ import pages.CategoryGuidancePage
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import queries.RecordCategorisationsQuery
-import repositories.SessionRepository
 import services.{AuditService, CategorisationService}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import utils.SessionData.{dataUpdated, pageUpdated}
