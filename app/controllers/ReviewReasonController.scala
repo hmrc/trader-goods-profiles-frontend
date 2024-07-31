2c5da36c69fe3ed1dc06bbd4b1a845a5078434be
Ewan-James-Donovan <34974028+Ewan-James-Donovan@users.noreply.github.com>
2024-07-31 16:07:31 +0100

    TGP-1961: Cleanup

--- app/controllers/ReviewReasonController.scala
+++ app/controllers/ReviewReasonController.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import connectors.GoodsRecordConnector
@@ -21,7 +37,7 @@ class ReviewReasonController @Inject()(
                                        goodsRecordConnector: GoodsRecordConnector
                                      )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
-  def onPageLoad(recordId: String): Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad(recordId: String): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request => {
       goodsRecordConnector.getRecord(request.eori, recordId).map { getGoodsRecordResponse =>
         if (getGoodsRecordResponse. toReview) {
