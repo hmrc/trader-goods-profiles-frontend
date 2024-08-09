4f0367df02dadbfa65243c85b95136faf2c51dfa
omotrium <omo4adex@gmail.com>
2024-08-09 10:29:25 +0100

    TGP-1976: Create page not found

--- app/controllers/PageNotFoundController.scala
+++ app/controllers/PageNotFoundController.scala
@@ -20,8 +20,7 @@ import controllers.actions._
 
 import javax.inject.Inject
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Request}
-import play.api.routing.Router.empty.routes
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.PageNotFoundView
 
