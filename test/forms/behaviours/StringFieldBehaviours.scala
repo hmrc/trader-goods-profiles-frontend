f83ccbe8cd20417545589f90902ed30ef2376f6e
vinoocs <158105847+vinoocs@users.noreply.github.com>
2024-08-13 13:16:08 +0100

    Fix for random build failure

--- test/forms/behaviours/StringFieldBehaviours.scala
+++ test/forms/behaviours/StringFieldBehaviours.scala
@@ -24,7 +24,7 @@ trait StringFieldBehaviours extends FieldBehaviours {
   def fieldWithMaxLength(form: Form[_], fieldName: String, maxLength: Int, lengthError: FormError): Unit =
     s"not bind strings longer than $maxLength characters" in {
 
-      forAll(stringsLongerThan(maxLength) -> "longString") { string =>
+      forAll(stringsWithMaxLength(maxLength) -> "longString") { string =>
         val result = form.bind(Map(fieldName -> string)).apply(fieldName)
         result.errors must contain only lengthError
       }
