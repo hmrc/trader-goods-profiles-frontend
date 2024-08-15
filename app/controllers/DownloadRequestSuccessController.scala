acf859d7ccdbfd5bdc020a53701a880c52009272
vinoocs <158105847+vinoocs@users.noreply.github.com>
2024-08-15 15:34:20 +0100

    TGP-1970 :updated

--- app/controllers/DownloadRequestSuccessController.scala
+++ app/controllers/DownloadRequestSuccessController.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import controllers.actions._
@@ -7,17 +23,22 @@ import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.DownloadRequestSuccessView
 
-class DownloadRequestSuccessController @Inject()(
-                                       override val messagesApi: MessagesApi,
-                                       identify: IdentifierAction,
-                                       getData: DataRetrievalAction,
-                                       requireData: DataRequiredAction,
-                                       val controllerComponents: MessagesControllerComponents,
-                                       view: DownloadRequestSuccessView
-                                     ) extends FrontendBaseController with I18nSupport {
+class DownloadRequestSuccessController @Inject() (
+  override val messagesApi: MessagesApi,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  profileAuth: ProfileAuthenticateAction,
+  val controllerComponents: MessagesControllerComponents,
+  view: DownloadRequestSuccessView
+) extends FrontendBaseController
+    with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad: Action[AnyContent] = (identify andThen profileAuth andThen getData andThen requireData) {
     implicit request =>
-      Ok(view())
+      // TODO : should be replaced with actual email & date
+      val email             = "sombody@email.com"
+      val downloadUntilDate = "18 August 2024"
+      Ok(view(email, downloadUntilDate))
   }
 }
