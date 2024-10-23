2be939c7d63aa781a6ce9abcef6b1b7ce5d6dbfe
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2024-10-23 13:50:27 +0100

    TGP-2698: cya page added for new UKIMS number

---
+++ app/controllers/CyaNewUkimsNumberController.scala
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import cats.data
+import cats.data.EitherNec
+import connectors.TraderProfileConnector
+import controllers.actions._
+import models.{NormalMode, TraderProfile, ValidationError}
+import navigation.Navigator
+import pages._
+import play.api.i18n.MessagesApi
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.AuditService
+import utils.SessionData._
+import viewmodels.checkAnswers._
+import viewmodels.govuk.summarylist._
+import views.html.CyaNewUkimsNumberView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class CyaNewUkimsNumberController @Inject() (
+  override val messagesApi: MessagesApi,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  profileAuth: ProfileAuthenticateAction,
+  val controllerComponents: MessagesControllerComponents,
+  view: CyaNewUkimsNumberView,
+  traderProfileConnector: TraderProfileConnector,
+  navigator: Navigator,
+  auditService: AuditService
+)(implicit ec: ExecutionContext)
+    extends BaseController {
+
+  private val errorMessage: String = "Unable to update new Ukims number in Trader profile."
+
+  def onPageLoad(): Action[AnyContent] = (identify andThen profileAuth andThen getData andThen requireData) {
+    implicit request =>
+      TraderProfile.validateUkimsNumber(request.userAnswers) match {
+        case Right(_)     =>
+          val list = SummaryListViewModel(
+            rows = Seq(
+              UkimsNumberSummary.rowUpdate(request.userAnswers)
+            ).flatten
+          )
+          Ok(view(list))
+        case Left(errors) =>
+          logErrorsAndContinue(errorMessage, routes.ProfileController.onPageLoad(), errors)
+      }
+  }
+
+  def onSubmit(): Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    (for {
+      ukimsNumber      <- handleValidateError(TraderProfile.validateUkimsNumber(request.userAnswers))
+      oldTraderProfile <- traderProfileConnector.getTraderProfile(request.eori)
+      newTraderProfile <- Future.successful(oldTraderProfile.copy(ukimsNumber = ukimsNumber, eoriChanged = false))
+      _                 = auditService.auditMaintainProfile(oldTraderProfile, newTraderProfile, request.affinityGroup)
+      _                <- traderProfileConnector.submitTraderProfile(newTraderProfile, request.eori)
+    } yield Redirect(navigator.nextPage(CyaMaintainProfilePage, NormalMode, request.userAnswers))).recover {
+      case e: TraderProfileBuildFailure =>
+        logErrorsAndContinue(e.getMessage, routes.ProfileController.onPageLoad())
+    }
+  }
+
+  private def handleValidateError[T](result: EitherNec[ValidationError, T]): Future[T] =
+    result match {
+      case Right(value) => Future.successful(value)
+      case Left(errors) => Future.failed(TraderProfileBuildFailure(errors))
+    }
+
+  private case class TraderProfileBuildFailure(errors: data.NonEmptyChain[ValidationError]) extends Exception {
+    private val errorsAsString      = errors.toChain.toList.map(_.message).mkString(", ")
+    override def getMessage: String = s"$errorMessage Missing pages: $errorsAsString"
+  }
+}
