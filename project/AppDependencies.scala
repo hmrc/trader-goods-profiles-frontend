e9b4492621909d19f83a8ff4226e221e61188307
Reece-Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2024-11-01 10:24:23 +0000

    TGP-2851: Update dependencies to latest versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,21 +2,21 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.5.0"
-  private val hmrcMongoVersion = "1.8.0"
+  private val bootstrapVersion = "9.5.0"
+  private val hmrcMongoVersion = "2.3.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "9.5.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "11.2.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "org.typelevel"     %% "cats-core"                  % "2.3.0"
+    "org.typelevel"     %% "cats-core"                  % "2.12.0"
   )
 
   private val test           = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.17.0"
+    "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
