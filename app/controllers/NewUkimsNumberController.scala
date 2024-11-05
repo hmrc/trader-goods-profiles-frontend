a61e6f68d9884c18270eeb56eabbf36138feb1fa
paul.olabisi <162993468+pauloncast@users.noreply.github.com>
2024-11-05 10:27:55 +0000

    TGP-2699: Added new controller and page for NewUkimsNumber

---
+++ app/controllers/NewUkimsNumberController.scala
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import connectors.TraderProfileConnector
+import controllers.actions._
+import forms.UkimsNumberFormProvider
+import models.Mode
+import navigation.Navigator
+import pages.NewUkimsNumberPage
+import play.api.data.{Form, FormError}
+import play.api.i18n.{Messages, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import views.html.{NewUkimsNumberView, UkimsNumberView}
+
+import javax.inject.Inject
+import scala.concurrent.Future
+
+class NewUkimsNumberController @Inject() (
+  override val messagesApi: MessagesApi,
+  sessionRepository: SessionRepository,
+  navigator: Navigator,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  formProvider: UkimsNumberFormProvider,
+  traderProfileConnector: TraderProfileConnector,
+  val controllerComponents: MessagesControllerComponents,
+  view: UkimsNumberView,
+  newView: NewUkimsNumberView
+)
+    extends BaseController {
+
+  private val form = formProvider()
+
+  def onPageLoadCreate(mode: Mode): Action[AnyContent] =
+    (identify andThen getData andThen requireData) { implicit request =>
+      val preparedForm = request.userAnswers.get(NewUkimsNumberPage) match {
+        case None        => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(newView(preparedForm, routes.NewUkimsNumberController.onSubmitCreate(mode), isCreateJourney = true))
+    }
+
+  def onSubmitCreate(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+    implicit request =>
+      form
+        .bindFromRequest()
+        .fold(
+          formWithErrors =>
+            Future
+              .successful(BadRequest(newView(formWithErrors, routes.NewUkimsNumberController.onSubmitCreate(mode)))),
+          value =>
+            for {
+              profile        <- traderProfileConnector.getTraderProfile(request.eori)
+              updatedAnswers <- Future.fromTry(request.userAnswers.set(NewUkimsNumberPage, value))
+              _              <- sessionRepository.set(updatedAnswers)
+            } yield
+              if (value == profile.ukimsNumber) {
+                val formWithApiErrors =
+                  createFormWithErrors(form, value, "ukimsNumberChangeController.duplicateUkimsNumber")
+                BadRequest(view(formWithApiErrors, routes.NewUkimsNumberController.onSubmitCreate(mode)))
+              } else {
+                Redirect(navigator.nextPage(NewUkimsNumberPage, mode, updatedAnswers))
+              }
+        )
+  }
+
+  private def createFormWithErrors[T](form: Form[T], value: T, errorMessageKey: String, field: String = "value")(
+    implicit messages: Messages
+  ): Form[T] =
+    form
+      .fill(value)
+      .copy(errors = Seq(FormError(field, messages(errorMessageKey))))
+}
