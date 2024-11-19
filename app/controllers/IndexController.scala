9758c10a857418a8984c5c3783a9461afe4c84ab
besscerule <63503574+besscerule@users.noreply.github.com>
2024-11-19 12:53:41 +0000

    TGP-2700: added new ukims page code

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -37,23 +37,17 @@ class IndexController @Inject() (
     extends BaseController {
 
   def onPageLoad: Action[AnyContent] = identify.async { implicit request =>
-    def checkProfileAndContinue =
-      traderProfileConnector.checkTraderProfile(request.eori).flatMap {
-        case true  => eoriChanged(request)
-        case false => Future.successful(Redirect(controllers.profile.routes.ProfileSetupController.onPageLoad()))
-      }
-
     if (config.downloadFileEnabled) {
       downloadDataConnector.getEmail(request.eori).flatMap {
         case Some(_) =>
-          checkProfileAndContinue
+          eoriChanged(request)
         case None    =>
           Future.successful(
             Redirect(url"${config.customsEmailUrl}/manage-email-cds/service/trader-goods-profiles".toString)
           )
       }
     } else {
-      checkProfileAndContinue
+      eoriChanged(request)
     }
   }
 
