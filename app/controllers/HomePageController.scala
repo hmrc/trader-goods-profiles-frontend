9ab06c1daba6b0ba209c099199787df9504e6d2e
Reece-Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2025-02-27 07:51:17 +0000

    TGP-3200-1: Potential timeout solution

--- app/controllers/HomePageController.scala
+++ app/controllers/HomePageController.scala
@@ -17,18 +17,23 @@
 package controllers
 
 import connectors.{DownloadDataConnector, GoodsRecordConnector, TraderProfileConnector}
-import controllers.actions._
+import controllers.actions.*
 import models.DownloadDataStatus.FileReadyUnseen
 import models.GoodsRecordsPagination.firstPage
 import models.download.DownloadLinkText
-import models.{DownloadDataSummary, HistoricProfileData}
+import models.{DownloadDataSummary, HistoricProfileData, TimeoutException}
+import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.pattern.after
 import play.api.i18n.MessagesApi
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.binders.RedirectUrl
 import utils.SessionData.{newUkimsNumberPage, pageUpdated}
 import views.html.HomePageView
 
 import javax.inject.Inject
-import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.DurationInt
 
 class HomePageController @Inject() (
   override val messagesApi: MessagesApi,
@@ -43,32 +48,49 @@ class HomePageController @Inject() (
 )(implicit ec: ExecutionContext)
     extends BaseController {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen profileAuth andThen getOrCreate).async { implicit request =>
-    for {
-      downloadDataSummary <- downloadDataConnector.getDownloadDataSummary
-      verifiedEmail       <- downloadDataConnector.getEmail.map {
-                               case Some(_) => true
-                               case None    => false
-                             }
-      goodsRecords        <- goodsRecordConnector.getRecords(1, 1)
-      doesGoodsRecordExist = goodsRecords.exists(_.goodsItemRecords.nonEmpty)
-      historicProfileData <- traderProfileConnector.getHistoricProfileData(request.eori)
-    } yield {
-      val downloadLinkText            = DownloadLinkText(downloadDataSummary, doesGoodsRecordExist, verifiedEmail)
-      val showNewUkimsBanner: Boolean = request.session.get(pageUpdated).contains(newUkimsNumberPage)
-      val viewUpdateGoodsRecordsLink  = getViewUpdateRecordsLink(historicProfileData)
+  private def withTimeout[T](future: Future[T], timeout: FiniteDuration): Future[T] = {
+    implicit val system: ActorSystem = ActorSystem("TimeoutSystem")
+    val timeoutFuture = after(timeout, system.scheduler)(Future.failed(TimeoutException))
+    Future.firstCompletedOf(Seq(future, timeoutFuture))
+  }
 
-      Ok(
-        view(
-          downloadReady(downloadDataSummary),
-          downloadLinkText,
-          showNewUkimsBanner,
-          doesGoodsRecordExist,
-          viewUpdateGoodsRecordsLink
+  def onPageLoad: Action[AnyContent] = (identify andThen profileAuth andThen getOrCreate).async { implicit request =>
+    val result = withTimeout(goodsRecordConnector.getRecords(1, 1).flatMap { _ =>
+        for {
+          downloadDataSummary <- downloadDataConnector.getDownloadDataSummary
+          verifiedEmail <- downloadDataConnector.getEmail.map {
+            case Some(_) => true
+            case None => false
+          }
+          goodsRecords <- goodsRecordConnector.getRecords(1, 1)
+          doesGoodsRecordExist = goodsRecords.exists(_.goodsItemRecords.nonEmpty)
+          historicProfileData <- traderProfileConnector.getHistoricProfileData(request.eori)
+        } yield {
+          val downloadLinkText = DownloadLinkText(downloadDataSummary, doesGoodsRecordExist, verifiedEmail)
+          val showNewUkimsBanner: Boolean = request.session.get(pageUpdated).contains(newUkimsNumberPage)
+          val viewUpdateGoodsRecordsLink = getViewUpdateRecordsLink(historicProfileData)
+
+          Ok(
+            view(
+              downloadReady(downloadDataSummary),
+              downloadLinkText,
+              showNewUkimsBanner,
+              doesGoodsRecordExist,
+              viewUpdateGoodsRecordsLink
+            )
+          )
+            .removingFromSession(pageUpdated)
+        }
+    }, 1.second).recoverWith {
+      case TimeoutException => Future.successful(
+        Redirect(
+          controllers.goodsProfile.routes.GoodsRecordsLoadingController
+            .onPageLoad(Some(RedirectUrl(controllers.routes.HomePageController.onPageLoad().url)))
         )
       )
-        .removingFromSession(pageUpdated)
     }
+
+    result
   }
 
   private def downloadReady(downloadDataSummary: Seq[DownloadDataSummary]): Boolean =
