28e11f1029c6d725670db733fd7012002c20c892
Reece-Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2025-04-01 10:07:48 +0100

    TGP-3286: Scala fmt

--- app/models/ott/package.scala
+++ app/models/ott/package.scala
@@ -18,18 +18,16 @@ package models
 
 package object ott {
   implicit class CategoryAssessmentSeqOps(assessments: Seq[CategoryAssessment]) {
-    def category1ToAnswer(isTraderNiphlAuthorised: Boolean): Seq[CategoryAssessment] = {
+    def category1ToAnswer(isTraderNiphlAuthorised: Boolean): Seq[CategoryAssessment] =
       assessments
         .filter(ass => !ass.hasNoAnswers)
         .filter(ass => !(ass.isNiphlAnswer && isTraderNiphlAuthorised))
         .filter(ass => !ass.onlyContainsNiphlAnswer)
-    }
 
-    def category2ToAnswer(isTraderNirmsAuthorised: Boolean): Seq[CategoryAssessment] = {
+    def category2ToAnswer(isTraderNirmsAuthorised: Boolean): Seq[CategoryAssessment] =
       assessments
         .filter(ass => !ass.hasNoAnswers)
         .filter(ass => !(ass.isNirmsAnswer && isTraderNirmsAuthorised))
         .filter(ass => !ass.onlyContainsNirmsAnswer)
-    }
   }
 }
