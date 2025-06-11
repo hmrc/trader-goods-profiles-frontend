9aff157f2b289a57aa4b7e0f6911833a58e83f7f
kenneth-rayner <31237148+kenneth-rayner@users.noreply.github.com>
2025-06-11 09:45:28 +0100

    update app dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.1.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.2.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "org.typelevel"     %% "cats-core"                  % "2.13.0",
