a408ae0e9ae80edb6b5254416a354926d6c777ad
kenneth-rayner <31237148+kenneth-rayner@users.noreply.github.com>
2025-06-11 19:01:59 +0100

    TGP-3499: update commodity code for when no change

--- app/controllers/goodsRecord/CyaUpdateRecordController.scala
+++ app/controllers/goodsRecord/CyaUpdateRecordController.scala
@@ -302,39 +302,39 @@ class CyaUpdateRecordController @Inject() (
   def onSubmitCountryOfOrigin(recordId: String): Action[AnyContent] =
     (identify andThen profileAuth andThen getData andThen requireData).async { implicit request =>
       val resultFuture = for {
-        oldRecord <- goodsRecordConnector.getRecord(recordId)
-        countryOfOrigin <-
+        oldRecord                <- goodsRecordConnector.getRecord(recordId)
+        countryOfOrigin          <-
           handleValidateError(
             UpdateGoodsRecord.validateCountryOfOrigin(request.userAnswers, recordId, oldRecord.category.isDefined)
           )
-        updateGoodsRecord <-
+        updateGoodsRecord        <-
           Future.successful(UpdateGoodsRecord(request.eori, recordId, countryOfOrigin = Some(countryOfOrigin)))
-        putGoodsRecord <- Future.successful(
-          PutRecordRequest(
-            actorId = oldRecord.eori,
-            traderRef = oldRecord.traderRef,
-            comcode = oldRecord.comcode,
-            goodsDescription = oldRecord.goodsDescription,
-            countryOfOrigin = countryOfOrigin,
-            category = None,
-            assessments = oldRecord.assessments,
-            supplementaryUnit = oldRecord.supplementaryUnit,
-            measurementUnit = oldRecord.measurementUnit,
-            comcodeEffectiveFromDate = oldRecord.comcodeEffectiveFromDate,
-            comcodeEffectiveToDate = oldRecord.comcodeEffectiveToDate
-          )
-        )
-        _ = auditService.auditFinishUpdateGoodsRecord(recordId, request.affinityGroup, updateGoodsRecord)
-        _ <- updateGoodsRecordIfPutValueChanged(
-          countryOfOrigin,
-          oldRecord.countryOfOrigin,
-          updateGoodsRecord,
-          putGoodsRecord
-        )
+        putGoodsRecord           <- Future.successful(
+                                      PutRecordRequest(
+                                        actorId = oldRecord.eori,
+                                        traderRef = oldRecord.traderRef,
+                                        comcode = oldRecord.comcode,
+                                        goodsDescription = oldRecord.goodsDescription,
+                                        countryOfOrigin = countryOfOrigin,
+                                        category = None,
+                                        assessments = oldRecord.assessments,
+                                        supplementaryUnit = oldRecord.supplementaryUnit,
+                                        measurementUnit = oldRecord.measurementUnit,
+                                        comcodeEffectiveFromDate = oldRecord.comcodeEffectiveFromDate,
+                                        comcodeEffectiveToDate = oldRecord.comcodeEffectiveToDate
+                                      )
+                                    )
+        _                         = auditService.auditFinishUpdateGoodsRecord(recordId, request.affinityGroup, updateGoodsRecord)
+        _                        <- updateGoodsRecordIfPutValueChanged(
+                                      countryOfOrigin,
+                                      oldRecord.countryOfOrigin,
+                                      updateGoodsRecord,
+                                      putGoodsRecord
+                                    )
         updatedAnswersWithChange <- Future.fromTry(request.userAnswers.remove(HasCountryOfOriginChangePage(recordId)))
-        updatedAnswers <- Future.fromTry(updatedAnswersWithChange.remove(CountryOfOriginUpdatePage(recordId)))
-        autoCategoriseScenario <- autoCategoriseService.autoCategoriseRecord(recordId, updatedAnswers)
-        _ <- sessionRepository.set(updatedAnswers)
+        updatedAnswers           <- Future.fromTry(updatedAnswersWithChange.remove(CountryOfOriginUpdatePage(recordId)))
+        autoCategoriseScenario   <- autoCategoriseService.autoCategoriseRecord(recordId, updatedAnswers)
+        _                        <- sessionRepository.set(updatedAnswers)
       } yield
         if (autoCategoriseScenario.isDefined || countryOfOrigin == oldRecord.countryOfOrigin) {
           // Redirect to SingleRecordController if auto categorised or no change in country
