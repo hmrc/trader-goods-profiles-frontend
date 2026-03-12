b30fc477e32f9cf7c8a6f12ad09b2d18d4cc009a
erickbm6922 <47341247+erickbm6922@users.noreply.github.com>
2026-03-12 11:36:06 +0000

    DDCE-8226: Updating dependecies to address vulenarabilities and also update the year in the copyright header

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
   private val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
